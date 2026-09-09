f5cca5f613a94fbbb24c876415e0e7071c861a5c
brai2003 <bautiflores428@gmail.com>
2026-09-09 08:38:59 -0300

    first commit

---
+++ calculadora/script.js
@@ -0,0 +1,29 @@
+const pantalla = document.getElementById('pantalla');
+
+function agregar(valor) {
+    if (pantalla.innerHTML === '0') {
+        pantalla.innerHTML = valor;
+    } else {
+        pantalla.innerHTML += valor;
+    }
+}
+
+function calcular() {
+    try {
+        pantalla.innerHTML = eval(pantalla.innerHTML);
+    } catch {
+        pantalla.innerHTML = "Error";
+    }
+}
+
+function borrar() {
+    if (pantalla.innerHTML.length > 1) {
+        pantalla.innerHTML = pantalla.innerHTML.slice(0, -1);
+    } else {
+        pantalla.innerHTML = '0';
+    }
+}
+
+function limpiar() {
+    pantalla.innerHTML = '0';
+}
